4bf1c24f53d2b6d2d6c100f89449f3e8dcb52826
copper-glitch <142946597+copper-glitch@users.noreply.github.com>
2025-11-12 20:19:58 -0800

    Update test.py

--- test.py
+++ test.py
@@ -25,14 +25,21 @@ mp_pose = mp.solutions.pose
 pose = mp_pose.Pose()
 mpDraw = mp.solutions.drawing_utils
 
-def getdata(path,name): 
-    video = os.path.join(path,name) 
-    cap = cv2.VideoCapture(video)
+def getdata(model): 
+    
+    cap = cv2.VideoCapture(0)
+    
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH,1920)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT,1080)
+    
     frames = []
     while True:
         spose = []
         success,img = cap.read()
+        
         if success != True:
+            break
+        if cv2.waitKey(10) & 0xFF == ord('q'):
             break
         imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         results = pose.process(imgRGB)
@@ -43,48 +50,23 @@ def getdata(path,name):
                 cx, cy = int(lm.x * w), int(lm.y * h)
                 spose.extend([cx,cy])
         cv2.imshow("Image", img)
-        key = cv2.waitKey(1) & 0xFF
         frames.append(spose)
-    return frames
-def pad(data):
-    if len(data)>MAX_F:
-        return data[:MAX_F]
-    elif len(data)<MAX_F:
-        pad = []
-        i=0
-        while i<MAX_F-len(data):
-            temp = []
-            x = 0
-            while x<FEATURES:
-                temp.append(0)
-                x+=1
-            pad.append(temp)
-            i+=1
-        pad=np.array(pad)
+        if len(frames)==MAX_F:
+            data = np.expand_dims(frames, axis=0).astype(np.float32)
+            output = model(data)
+            guess = np.argmax(output)
+            print(guess)
+            frames=[]
+            
+            
+            
         
-        # ___________________________________________
-        return np.vstack((data,pad))
-        # ____________________________________________
-    else:
-        return data   
 def main():
-    path = 'C:/Users/ninja/Desktop/Capstone/mvp/'
-    
-    with open(sys.argv[1], 'rb') as file:  
+    if len(sys.argv)!=1:
+        with open(sys.argv[1], 'rb') as file:  
             model = pickle.load(file)
-
-    for name in os.listdir(path):
-
-        print(name)
-        data = getdata(path,name)
-        data=np.array(pad(data))
-        #_________________________________________________________  
-        data = np.expand_dims(data, axis=0).astype(np.float32)
-        #_________________________________________________________
-        
-        output = model(data)
-        guess = np.argmax(output)
-        print(guess)
-    
-    cv2.destroyAllWindows()
+        getdata(model)
+        cv2.destroyAllWindows()
+    else:
+        print("Need Command Line arguement: Model pkl file")
 main()
